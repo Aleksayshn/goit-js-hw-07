86823493e68ce047a5f44223a5b6f239045912a6
Aleksandra <ag7756862@gmail.com>
2022-12-27 17:36:45 +0200

    feat: add solutionTask1

---
+++ js/01-gallery.js
@@ -0,0 +1,43 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+
+const galleryItemsEl = document.querySelector('.gallery');
+
+const galleryMarkup = createGalleryImage(galleryItems);
+galleryItemsEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryItemsEl.addEventListener('click', onImageClick)
+
+function createGalleryImage(images) {
+    return images
+    .map(({preview, original, description}) => {
+           return`
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="large-image.jpg"
+      alt="${description}"
+    />
+  </a>
+</div> `;
+    })
+    .join('');    
+}
+
+function onImageClick(ev) {
+if (ev.target.nodeName !== 'IMG') {
+    return
+} 
+
+ev.preventDefault();
+
+const largeImage = ev.target.closest('.gallery__link').href;
+console.log(largeImage);
+
+  const instance = basicLightbox.create(`
+    <img src="${largeImage}" width="800" height="600">`)
+    .show();
+}    
