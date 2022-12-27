04d34a5f40131a0731b0a9a0e5bedaaf0e4c09b3
Aleksandra <ag7756862@gmail.com>
2022-12-27 23:58:19 +0200

    feat: add library simplelightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,34 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryItemsEl = document.querySelector('.gallery');
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryItemsEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.reduce((acc, { original, preview, description }) => 
+    acc += `<a class="gallery__item" href="${original}">
+            <img 
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}" 
+            />
+        </a>`, "");
+}
+
+const lightbox = new SimpleLightbox (".gallery a", {
+    captionSelector: ".gallery__image", 
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+
+//   window.addEventListener('keydown', onEscKeyPress)
+
+//   function onEscKeyPress(ev) {
+//     if (ev.code === 'Escape') {
+//     lightbox.close();
+//     }
+//   }
+
